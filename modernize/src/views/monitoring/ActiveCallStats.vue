32c3570d8fc18237d17160c38abf65aac6373802
natty0337 <natty0337@arisys.co.kr>
2025-03-11 18:39:02 +0900

    chore: 활성통화건수 모니터링 페이지 도넛차트 너비 굵게 변경 #4

--- modernize/src/views/monitoring/ActiveCallStats.vue
+++ modernize/src/views/monitoring/ActiveCallStats.vue
@@ -159,7 +159,7 @@ const donutchartOptions = computed(() => {
         plotOptions: {
             pie: {
                 donut: {
-                    size: '70px'
+                    size: '50px',
                 }
             }
         },
