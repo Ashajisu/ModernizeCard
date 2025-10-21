fe3ac121867f198a792c686a5b3bca6068434b2c
jisu_arisys <jisu_um@arisys.co.kr>
2025-10-21 17:35:53 +0900

    fix : 경로수정
    TypeError: Failed to fetch dynamically imported module: https://jisu-arisys.github.io/ModernizeCard/assets/FullLayout.js

--- modernize/vite.config.ts
+++ modernize/vite.config.ts
@@ -1,7 +1,4 @@
-
-
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from "vite-plugin-vuetify";
@@ -33,7 +30,7 @@ export default defineConfig({
     },
       allowedHosts: ['localhost','ashajisu.iptime.org']
   },
-  base: 'ModernizeCard',
+  base: '/ModernizeCard/',
   build: {
     outDir : '../dist',
     emptyOutDir: true,            // 해당 경로 이전 build 파일 삭제
