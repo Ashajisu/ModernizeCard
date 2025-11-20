106a424072995ad8cfcb46cc5ba23d232c63cb6f
jisu_arisys <jisu_um@arisys.co.kr>
2025-11-20 16:36:06 +0900

    Updates

--- assets/gallery-DpV2etG7.js
+++
@@ -1,1 +0,0 @@
-import{aX as a,aZ as r}from"./index-CG8jbmZw.js";const l=a({id:"Gallery",state:()=>({gallery:[]}),getters:{},actions:{async fetchGallery(){try{const e=await r.get("/api/gallery/list");this.gallery=e.data.gallery}catch(e){alert(e),console.log(e)}}}});export{l as u};
