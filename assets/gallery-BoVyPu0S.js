90318d2c75a644fe1625dc0b16bdb0d7c0647ee9
jisu_arisys <jisu_um@arisys.co.kr>
2026-01-27 15:24:17 +0900

    Updates

--- assets/gallery-BoVyPu0S.js
+++
@@ -1,1 +0,0 @@
-import{aZ as a,a$ as r}from"./index-BLjmS4Jq.js";const l=a({id:"Gallery",state:()=>({gallery:[]}),getters:{},actions:{async fetchGallery(){try{const e=await r.get("/api/gallery/list");this.gallery=e.data.gallery}catch(e){alert(e),console.log(e)}}}});export{l as u};
