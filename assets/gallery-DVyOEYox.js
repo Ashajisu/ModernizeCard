1be835695add680a9539fb1d9b9b3f9f249d294a
jisu-arisys <jisu-arisys@users.noreply.github.com>
2026-07-29 07:01:14 +0000

    deploy: 2c0b88f8cc64c1a1fd2c1adf8e566ad27e00889e

---
+++ assets/gallery-DVyOEYox.js
@@ -0,0 +1,1 @@
+import{a_ as a,b0 as r}from"./index-CYgT0-Ra.js";const l=a({id:"Gallery",state:()=>({gallery:[]}),getters:{},actions:{async fetchGallery(){try{const e=await r.get("/api/gallery/list");this.gallery=e.data.gallery}catch(e){alert(e),console.log(e)}}}});export{l as u};
