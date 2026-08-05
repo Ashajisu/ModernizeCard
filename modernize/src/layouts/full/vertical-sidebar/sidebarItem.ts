9c7bd020e4ad0b247bae49c1a6c3642572d7a94c
Ashajisu <asha.jisu@gmail.com>
2026-08-05 17:15:58 +0900

    fix: 아이콘 수정

--- modernize/src/layouts/full/vertical-sidebar/sidebarItem.ts
+++ modernize/src/layouts/full/vertical-sidebar/sidebarItem.ts
@@ -1,21 +1,18 @@
 import {
-    //https://tabler.io/icons 참고
+    //https://tabler.io/icons 는 이미지만 참고
+    //이 라이브러리 하위에서 고르기
     ApertureIcon,
+    BrandLinkedinIcon,
+    CircuitCellPlusIcon,
     CreditCardIcon,
-    PlusMinusIcon
+    FocusAutoIcon,
+    FolderShareIcon,
+    ListDetailsIcon,
+    ListIcon,
+    PlusMinusIcon,
+    SquareChevronRightIcon,
+    WashDryAIcon
 } from 'vue-tabler-icons';
-import { 
-    IconBrandLinkedin,
-    IconChevronRight,
-    IconMenu2,
-    IconFocusAuto,
-    IconLayoutDashboard,
-    IconListDetails,
-    IconPinInvoke,
-    IconCircuitCellPlus,
-    IconWashDryA
-} from '@tabler/icons-vue';
-
 
 export interface menu {
     header?: string;
@@ -83,42 +80,42 @@ const sidebarItem: menu[] = [
     { header: '복식부기' },
     {
         title: '대차대조표',
-        icon: IconCircuitCellPlus,
+        icon: CircuitCellPlusIcon,
         to: '/journal/balancesheet'
     },
     {
         title: '손익계산서',
-        icon: IconBrandLinkedin,
+        icon: BrandLinkedinIcon,
         to: '/journal/incomestatement'
     },
     {
         title: '초기잔액등록',
-        icon: IconPinInvoke,
+        icon: FolderShareIcon,
         to: '/journal/openingbalance'
     },
     {
         title: '전표 수기입력',
-        icon: IconChevronRight,
+        icon: SquareChevronRightIcon,
         to: '/journal/manualentry'
     },
     {
         title: '전표조회',
-        icon: IconMenu2,
+        icon: ListIcon,
         to: '/journal/list'
     },
     {
         title: '계정과목관리',
-        icon: IconWashDryA,
+        icon: WashDryAIcon,
         to: '/journal/accountmanagement'
     },
     {
         title: '계정별 잔액',
-        icon: IconFocusAuto,
+        icon: FocusAutoIcon,
         to: '/journal/accountbalance'
     },
     {
         title: '월별 예산/지출통계',
-        icon: IconListDetails,
+        icon: ListDetailsIcon,
         to: '/journal/budgetstats'
     }
 ];
