efa2d496ed441f49000c5838c4b970982f2afced
jisu-arisys <jisu-arisys@users.noreply.github.com>
2026-08-10 07:38:02 +0000

    deploy: 0441051382fdab0abf40d958f6f9d211ccb14466

---
+++ assets/friends-DpH1-VYJ.js
@@ -0,0 +1,1 @@
+import{b5 as s,b7 as r}from"./index-DbxxH51V.js";const i=s({id:"Frineds",state:()=>({friends:[]}),getters:{},actions:{async fetchFrineds(){try{const e=await r.get("/api/friends/list");this.friends=e.data.friends}catch(e){alert(e),console.log(e)}}}});export{i as u};
